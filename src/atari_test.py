6f27e7b4d66d192aad2e838549e6581fb618effa
Filip Langr <zerg502@gmail.com>
2019-05-27 15:32:26 +1000

    Testing improved.

--- src/atari_test.py
+++ src/atari_test.py
@@ -7,6 +7,8 @@ from cartesian.cgp import *
 import time
 
 def test_final(f, timesteps=1000, timesleep=None):
+    white_reward = 0
+    black_reward = 0
     env = gym.make(config.gym_params['game_name']).env
     observation = env.reset()
     observation = observation / 255.0
@@ -19,11 +21,18 @@ def test_final(f, timesteps=1000, timesleep=None):
         # Get the index of highest value, it's our action.
         action = np.argmax(values)
         # Play the action.
+        #action = np.random.randint(0, 18)
         observation, reward, done, _ = env.step(action)
+        reward = int(reward)
+        if reward > 0:
+            white_reward += reward
+        elif reward < 0:
+            black_reward += reward
         observation = observation / 255.0
         if done:
             break
     env.render()
+    print("White %d : %d Black" % (white_reward, -black_reward))
     input("Press any key to exit.")
     env.close()
 
